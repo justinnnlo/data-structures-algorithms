4de4398cd987cad474e6958da27bb520f46155fe
Justin Lo <justinkevinlo@gmail.com>
2020-03-21 20:17:20 -0700

    added two pointer front end swap method

--- 344.reversestring.py
+++ 344.reversestring.py
@@ -3,9 +3,17 @@ def reverseString(s):
     """
     Do not return anything, modify s in-place instead.
     """
-    for i in range(len(s) // 2):
-        s[i], s[-i - 1] = s[-i - 1], s[i]
+    # for i in range(len(s) // 2):
+    #     s[i], s[-i - 1] = s[-i - 1], s[i]
     # return s
+
+    # 2 pointer swap method
+    start, end = 0, len(s) - 1
+
+    while start < end:
+        s[start], s[end] = s[end], s[start]
+        start += 1
+        end -= 1
 
 
 print(reverseString(["h", "e", "l", "l", "o"]))
