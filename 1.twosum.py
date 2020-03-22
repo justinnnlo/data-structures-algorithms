295c6d446cf422306a72213ea9bdee2c8074355d
Justin Lo <justinkevinlo@gmail.com>
2020-03-21 19:12:02 -0700

    twosum return index solution

---
+++ 1.twosum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+      cache = {}
+      
+      for i in range(len(nums)):
+        num = nums[i]
+        match = target - num
+        if match in cache.keys(): 
+          return [cache[match], i]
+        else:
+          cache[num] = i
+      return
+
+print(twoSum([2, 7, 11, 15], 9))
