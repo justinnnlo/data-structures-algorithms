6712c4be4db96bb2779bc3ea9f72f5c6879930f6
Justin Lo <justinkevinlo@gmail.com>
2020-03-21 21:01:26 -0700

    buy and sell stocks I

---
+++ 121.besttimetobuyandsellstock.py
@@ -0,0 +1,12 @@
+def maxProfit(prices):
+	  if not prices:
+        return 0
+    max_profit, min_price = 0, prices[0]
+    for price in prices:
+        min_price = price if price < min_price else min_price
+        max_profit = price - min_price if price - \
+            min_price > max_profit else max_profit
+    return max_profit
+
+
+print(maxProfit([7, 1, 5, 3, 6, 4]))
