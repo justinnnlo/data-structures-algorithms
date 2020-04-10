14fd918d547c16b674e67e458fc4ee8d8e36f44b
Justin Lo <justinkevinlo@gmail.com>
2020-04-09 17:41:55 -0700

    clean up

--- LeetCode/36.valid_sudoku.py
+++ LeetCode/36.valid_sudoku.py
@@ -1,1 +1,11 @@
+''' 9 x 9 sudoku board '''
+
+
 def is_valid_sudoku(board):
+    seen = []
+
+    for i, row in enumerate(board):
+        for j, c in enumerate(row):
+            if c != '.':
+                seen += [(c, j), (i, c), (i/3, j/3, c)]
+    return len(seen) == len(set(seen))
---
+++ LeetCode/75.sort_colors.py
@@ -0,0 +1,2 @@
+def sort_colors(list):
+    
